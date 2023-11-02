9f2e0fa1f2175285e5523fb08b09520e5a089ee0
jakesuellentrop <jakesuellentrop@gmail.com>
2023-11-02 12:15:10 +0000

    adds php dev aliases

--- .alias.sh
+++ .alias.sh
@@ -3,9 +3,11 @@
 alias artisan="php artisan"
 if [ -f /etc/hostname ]; then
 	alias artserv="php artisan serve --host=$(cat /etc/hostname)"
+	alias phpserv="php -S $(cat /etc/hostname):8000"
 fi
 alias pyserv="python manage.py runserver"
 alias lgit="lazygit"
 alias phpunit="vendor/bin/phpunit"
 alias pint="vendor/bin/pint"
+alias tb="vendor/bin/testbench"
 
